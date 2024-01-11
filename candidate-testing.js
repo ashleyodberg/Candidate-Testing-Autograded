3ff1c29f60b9f18784f507cb7aa9f083aee81670
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-11 11:38:36 -0800

    I believe I have successfully run the program as it is asking for my name, prompting me for an answer to the quiz, and grading the quiz and providing feedback so far for part 1 of the overall assignment.

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space?";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,26 +18,36 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("What is your first and last name? " );
+  candidateName = input.question("What is your first and last name? npm" );
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question + " ");
+ // console.log("You entered: " + candidateAnswer); to check myself here
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  if (candidateAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+    console.log("Your answer is correct!");
+    grade = 1; // assigning a score of 1 for a correct answer
+  } else {
+    console.log("Sorry, your answer is incorrect.");
+    grade = 0; //assign a score of 0 for an incorrect answer
+  }
 
+  return grade; //trying moving this here to see if it solves my issue
+}
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
-  return grade;
-}
+ // return grade; 
+
 
 function runProgram() {
   askForName();
