3863dda31eefc8b752467128ce153bcec0b05061
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-11 16:32:04 -0800

    removed a typo in the initialization of one of my variables, enabled actions on my github and am pushing changes to ensure successful update and implementation

--- candidate-testing.js
+++ candidate-testing.js
@@ -18,7 +18,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("What is your first and last name? npm" );
+  candidateName = input.question("What is your first and last name? " );
 }
 
 function askQuestion() {
