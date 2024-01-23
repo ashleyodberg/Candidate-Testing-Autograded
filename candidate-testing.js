3926c858c26f5f54809186dcda1299d4bde7979c
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-22 16:47:46 -0800

    worked through part 2 and passed all tests expected however 10 will not run successfully and I am stumped. Will be seeking help from IA and reviewing lecture slides for help with this

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,21 @@ let candidateAnswer = ""; //A variable containing an empty string has been initi
 
 //TODO: Variables for Part 2  
 //Here I will be initializing each of these variables to store multiple questions and multiple correct answers, as well as multiple user input responses to each question
-let questions; 
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometers = 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? " 
+]; 
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 function askForName() {
   // TODO 1.1b: Ask for candidate's name
   // This code block defines a function to prompt the user for their name (input) and store the received value in candidateName
@@ -19,29 +31,44 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer
   // this code block defines the askQuestion function which will prompt the user for an answer to the quiz question and store it in candidateAnswer
-  candidateAnswer = input.question(question + " ");
- // console.log("You entered: " + candidateAnswer); to check myself here
-}
-function gradeQuiz(candidateAnswers) { 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
-  // defines the gradeQuiz function which checks to see if a candidate's answer was correct, and then scores the answer with a 1 if it was correct or a 0 for incorrect
-  if (candidateAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
-    console.log("Your answer is correct!");
-    grade = 1; // assigning a score of 1 for a correct answer
-  } else {
-    console.log("Sorry, your answer is incorrect.");
-    grade = 0; //assign a score of 0 for an incorrect answer
+ // candidateAnswer = input.question(question + " ");
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswer = input.question(questions[i]);
+    candidateAnswers.push(candidateAnswer);
   }
-  return grade; //trying moving this here to see if it solves my issue
+  // console.log("You entered: " + candidateAnswer); to check myself here
 }
+function gradeQuiz(candidateAnswers) {
+  let totalCorrect = 0; //Initialize a variable to count correct answers input by user
+
+  for (let i = 0; i < questions.length; i++) {
+    console.log(`\n${i + 1}) ${questions[i]}`);
+    console.log(`Your Answer: ${candidateAnswers[i]}`);
+    console.log(`Correct Answer: ${correctAnswers[i]}`);
+    // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
+    // defines the gradeQuiz function which checks to see if a candidate's answer was correct, and then scores the answer with a 1 if it was correct or a 0 for incorrect
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      console.log("Your answer is correct!");
+      totalCorrect++;
+      // grade = 1; // assigning a score of 1 for a correct answer
+    } else {
+      console.log("Sorry, your answer is incorrect.");
+      // grade = 0; //assign a score of 0 for an incorrect answer
+    }
+  //  return grade; //trying moving this here to see if it solves my issue
+  }
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
- // return grade; 
+  grade = (totalCorrect / questions.length) * 100; //this line calculates the percentage of correct candidate answers they've input
+  
+  return grade;
+}
+
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
    console.log("Hello, " + candidateName + "!");
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  gradeQuiz(candidateAnswers);
 }
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
